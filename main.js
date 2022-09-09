fc3c14b198a9b83299dd444068fb9f263fa34dc5
Teva90 <mostafaayman551@gmail.com>
2022-09-10 01:08:56 +0200

    adding files

---
+++ main.js
@@ -0,0 +1,13 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGV2YTkwIiwiYSI6ImNsN3RqNXQ2czBqbDUzdmxlcTZqc3psZjAifQ.aLATAKK43Cd2KYpl2TRu_A';
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9, // starting zoom
+projection: 'globe' // display the map as a 3D globe
+});
+map.on('style.load', () => {
+map.setFog({}); // Set the default atmosphere style
+});
+
